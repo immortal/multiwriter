33808f299da4da38a7b33626b1432efa47fa9c35
nbari <nbari@tequila.io>
2016-07-17 21:32:27 +0200

    new file:   multiwriter.go

---
+++ multiwriter.go
@@ -0,0 +1,42 @@
+package multiwriter
+
+import "io"
+
+type multiWriter struct {
+	writers []io.Writer
+}
+
+// Write implements io.Writer
+func (self *multiWriter) Write(p []byte) (n int, err error) {
+	type result struct {
+		n   int
+		err error
+	}
+
+	rs := make(chan *result)
+
+	for _, w := range self.writers {
+		go func(writer io.Writer) {
+			n, err := writer.Write(p)
+			rs <- &result{n, err}
+		}(w)
+	}
+
+	for range self.writers {
+		r := <-rs
+		if r.err != nil {
+			return r.n, r.err
+		}
+		if r.n != len(p) {
+			return r.n, io.ErrShortWrite
+		}
+	}
+	return len(p), nil
+}
+
+// New return a new multiWriter
+func New(writers ...io.Writer) io.Writer {
+	w := make([]io.Writer, len(writers))
+	copy(w, writers)
+	return &multiWriter{w}
+}
